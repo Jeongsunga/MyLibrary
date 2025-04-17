318d9ad7091136b4af7f1cffac15ea657493bb61
Seunga Jeong <jsa3341@gmail.com>
2025-04-17 20:30:36 +0900

    env setting

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
+import Homepage from './pages/Homepage/Homepage';
+import Loginpage from './pages/Loginpage/Loginpage';
+import Mypage from './pages/Mypage/Mypage';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
+// 네비게이션(검색)
+// 도서 전체 페이지
+// 로그인 페이지
+// 좋아요 페이지
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path='/' element={<Homepage/>}/>
+      <Route path='/login' element={<Loginpage/>}/>
+      <Route path='/mypage' element={<Mypage/>}/>
+    </Routes>
   );
 }
 
